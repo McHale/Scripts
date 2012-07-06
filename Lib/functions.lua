c0cc8ddd28d2473935ac59d5499f949e9d0418df
McHale <lepuggy@gmail.com>
2012-07-05 22:23:25 -0400

    more formatting

---
+++ Lib/functions.lua
@@ -0,0 +1,14 @@
+--==========================================================
+-- Script Name: Common Functions
+-- Author: McHale
+-- Version: 0.1
+-- OpenEUO version tested with: 0.91
+-- Purpose: Hold Commonly Used Code Blocks as Functions
+-- and to simplifiy the arguments
+--==========================================================
+
+function errorMsg(msg)
+	UO.ExMsg(UO.CharID,3,33,msg)
+end
+
+
