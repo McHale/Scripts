c9768fd95942ee1a3a849480055acb4e10985f15
Themadxcow <themadxcow@gmail.com>
2012-11-22 15:23:06 -0700

    Lib addendum
    There is large amount of redundancy here.  I don't know if you want me
    to work on reducing the library down into specific collections.

--- Lib/functions.lua
+++ Lib/functions.lua
@@ -12,3 +12,27 @@ function errorMsg(msg)
 end
 
 
+-- Gump Collection
+
+function AllGumps()
+local a = {}
+	for i=0, math.huge do
+    local sName,nX,nY,nSX,nSY,nKind,nId,nType,nHP = UO.GetCont(i)
+		if sName ==nil then
+			return a, i
+		else
+			a[i] = string.lower({name=sName,x=nX,y=nY,sizex=nSX,sizey=nSY,kind=nKind,id=nId,type=nType,hp=nHP})
+		end
+	end
+end
+
+-- Backpack Check
+function PackCheck()
+local a, cnt = AllGumps()
+	for i=0, cnt-1 do
+		if a[i].id == UO.BackpackID then
+			return true
+		end
+	end
+	return false
+end
